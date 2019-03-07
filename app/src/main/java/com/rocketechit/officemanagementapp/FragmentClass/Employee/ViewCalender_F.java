f1756bb9bc7f1383a80fd933cf5b71934d2432d5
Imran sk <imran.bu96@gmail.com>
2019-03-07 12:49:11 +0600

    event showing complete

--- app/src/main/java/com/rocketechit/officemanagementapp/FragmentClass/Employee/ViewCalender_F.java
+++ app/src/main/java/com/rocketechit/officemanagementapp/FragmentClass/Employee/ViewCalender_F.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -37,6 +38,8 @@ public class ViewCalender_F extends Fragment {
     @BindView(R.id.date_TV)
     TextView dateTV;
 
+    static String userID_Company;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -50,12 +53,39 @@ public class ViewCalender_F extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         firebaseDatabase = FirebaseDatabase.getInstance();
         databaseReference = firebaseDatabase.getReference();
+//get Company User ID
+        getCompanyUserName(new MyCallback() {
+            @Override
+            public void onCallback(String value) {
+                userID_Company = value;
+                highlightedTheDate(userID_Company);
+            }
+        });
 
+        calenderViewID.setOnDateClickListener(new OnDateClickListener() {
+            @Override
+            public void onDateClick(View view, DateData date) {
+
+                String year = String.valueOf(date.getYear());
+                String month = String.valueOf(date.getMonth());
+                String day = String.valueOf(date.getDay());
+                String datew = String.format("%d, %d, %d", date.getYear(), date.getMonth(), date.getDay());
+                getCompanyUserName(value -> {
+                    userID_Company=value;
+                    checkTheDateIsTaken(datew, year, month, day,userID_Company);
+
+                });
+            }
+        });
+
+    }
+//highlighted The Date  Method is here
+private void highlightedTheDate(String userID_company) {
         //high lilted the event Date
         databaseReference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                for (DataSnapshot snapshot : dataSnapshot.child("Event_List").child("1bDbWQodBxXVX0mnKdERRa8vYuH3")
+                for (DataSnapshot snapshot : dataSnapshot.child("Event_List").child(userID_company)
                         .getChildren()) {
                     EventClass eventClass = snapshot.getValue(EventClass.class);
                     int year = Integer.parseInt(eventClass.getYear());
@@ -70,31 +100,41 @@ public class ViewCalender_F extends Fragment {
 
             }
         });
+    }
+//get Company User ID Method
 
-
-
-        calenderViewID.setOnDateClickListener(new OnDateClickListener() {
+    private String getCompanyUserName(MyCallback myCallback) {
+        databaseReference.addValueEventListener(new ValueEventListener() {
             @Override
-            public void onDateClick(View view, DateData date) {
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                userID_Company = dataSnapshot.child("Employee_List")
+                        .child(getUserID()).child("userID_company").getValue(String.class);
+                myCallback.onCallback(userID_Company);
+            }
 
-                String year = String.valueOf(date.getYear());
-                String month = String.valueOf(date.getMonth());
-                String day = String.valueOf(date.getDay());
-                String datew = String.format("%d, %d, %d", date.getYear(), date.getMonth(), date.getDay());
-                checkTheDateIsTaken(datew, year, month, day);
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
             }
         });
 
+        return userID_Company;
     }
 
+    //interface class for return the value from snapshot and ge the value in oncreate view
+    public interface MyCallback {
+        void onCallback(String value);
+    }
+
+
     //check the click date is available on the Event list
-    private void checkTheDateIsTaken(String datew, String year, String month, String day) {
+    private void checkTheDateIsTaken(String datew, String year, String month, String day,String userID_Company) {
 //high lilted the event Date
         databaseReference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
 
-                boolean hasTheDate = dataSnapshot.child("Event_List").child("1bDbWQodBxXVX0mnKdERRa8vYuH3")
+                boolean hasTheDate = dataSnapshot.child("Event_List").child(userID_Company)
                         .hasChild(datew);
                 if (hasTheDate) {
 
@@ -102,12 +142,12 @@ public class ViewCalender_F extends Fragment {
                         @Override
                         public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
 
-                            String title = dataSnapshot.child("Event_List").child("1bDbWQodBxXVX0mnKdERRa8vYuH3")
+                            String title = dataSnapshot.child("Event_List").child(userID_Company)
                                     .child(datew).child("eventTitle").getValue(String.class);
-                            String details = dataSnapshot.child("Event_List").child("1bDbWQodBxXVX0mnKdERRa8vYuH3")
+                            String details = dataSnapshot.child("Event_List").child(userID_Company)
                                     .child(datew).child("eventDetails").getValue(String.class);
 //this date taken only for use when i update the event
-                            String date = dataSnapshot.child("Event_List").child("1bDbWQodBxXVX0mnKdERRa8vYuH3")
+                            String date = dataSnapshot.child("Event_List").child(userID_Company)
                                     .child(datew).child("date").getValue(String.class);
                             dateTV.setText(date);
 
@@ -131,8 +171,7 @@ public class ViewCalender_F extends Fragment {
         });
     }
 
-
-   /* //get Current Admin User ID
+    //get Current Admin User ID
     private String getUserID() {
 
         FirebaseAuth firebaseAuth;
@@ -141,6 +180,6 @@ public class ViewCalender_F extends Fragment {
         firebaseUser = firebaseAuth.getCurrentUser();
         String userID = firebaseUser.getUid();
         return userID;
-    }*/
+    }
 
 }
